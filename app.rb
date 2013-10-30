c1b764878002612d4ba60028f6c10bdfee9b85c1
Daniel Kimbel <danielkimbel@Daniels-MacBook-Pro.local>
2013-10-29 18:41:11 -0700

    Implementing routes and views for posts and user pages; partway there.

--- app.rb
+++ app.rb
@@ -26,3 +26,19 @@ post '/sign_up' do
   User.create(:email => params[:email], :password => params[:password])
   erb :confirm_new_user
 end
+
+get '/:page_id' do
+  @feed_owner = User.find(params[:page_id])
+  halt 404 unless @feed_owner
+  # ideally make this page display form and content if and only 
+  # if owner is logged in, otherwise display content only. May
+  # need to accomplish this using two separate views.
+  erb :user_feed
+end
+
+post '/:page_id' do
+  # add something to authenticate that user is owner in order to post
+  Post.create(:title => params[:title], :body => params[:body],
+           :user_id => params[:page_id])
+  erb :user_feed
+end
