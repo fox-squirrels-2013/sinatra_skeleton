0514dc417adcca94846199239e9b260ed3b6abb4
Apprentice <apprentice@dbc26.local>
2013-10-29 18:42:07 -0700

    Authentication seemingly working.

--- app.rb
+++ app.rb
@@ -5,15 +5,27 @@ require_relative './app/models/user'
 ActiveRecord::Base.establish_connection(adapter: 'postgresql',
                                         database: 'social_network')
 
+set :session_secret, ENV["SESSION_KEY"] || 'too secret'
+
+enable :sessions
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
 get '/' do
-  erb :index
+  erb :index, :locals => { :session => session }
 end
 
 post '/' do
   curr_user = User.find_by email: params[:email]
   if !curr_user || curr_user.password != params[:password]
     erb :login_failed
+  elsif curr_user.password != params[:password]
+    erb :login_failed
   else
+    session[:email] = params[:email]
     erb :sign_in
   end
 end
@@ -25,4 +37,9 @@ end
 post '/sign_up' do
   User.create(:email => params[:email], :password => params[:password])
   erb :confirm_new_user
+end
+
+get '/logout' do 
+  session.clear
+  redirect '/'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131028185947) do
+ActiveRecord::Schema.define(version: 20131028185948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20131028185947) do
   create_table "friends", force: true do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
