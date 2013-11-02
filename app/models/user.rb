4f95ef001d9a2b197e79a099e44ea5c5a1ca5b72
Apprentice <apprentice@dbc08.local>
2013-11-01 18:22:45 -0700

    Working on validation...

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,4 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :friends
   has_many :friend_requests, :uniq => true
-  def self.from_email(email)
-    return User.all.find_by email: email
-  end
 end
