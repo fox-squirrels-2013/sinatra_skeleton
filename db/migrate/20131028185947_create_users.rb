951bf9b5487e79224ee218ee24002020146f3df2
Apprentice <apprentice@dbc08.local>
2013-11-01 18:34:51 -0700

    Fixed migration problem.

--- db/migrate/20131028185947_create_users.rb
+++ db/migrate/20131028185947_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table(:users) do |t|
       t.string :name
       t.string :email
-      t.string :hex_digest
+      t.string :password
       t.integer :friends_id
       t.timestamps
     end
